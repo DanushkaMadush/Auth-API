0be876b602b329ca1d61daa2572dd2043b71761e
danushkacodeolima <danushka@codeolima.com>
2024-11-02 13:59:07 +0530

    added data

---
+++ Data/AuthContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Auth_API.Models;
+
+namespace Auth_API.Data
+{
+    public class AuthContext : DbContext
+    {
+        public AuthContext(DbContextOptions<AuthContext> options) :base (options) { }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
