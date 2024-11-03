af56371cb21e054ffc6219311cb6d80444fa3c9b
danushkacodeolima <danushka@codeolima.com>
2024-11-03 21:05:49 +0530

    Added controllers

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,82 @@
+﻿using Auth_API.Data;
+using Auth_API.Models;
+using Auth_API.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthContext _context;
+
+        public AuthController(AuthContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            if (await _context.Users.AnyAsync(u => u.Username == request.Username))
+                return BadRequest("Username already exists");
+
+            CreatePasswordHash(request.Password, out byte[] passwordHash , out byte[] passwordSalt);
+
+            var user = new User
+            {
+                Username = request.Username,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwaordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwaordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == request.Username);
+
+            if (user == null)
+            {
+                return BadRequest("User Not Found");
+            }
+
+            if (!VerifyPasswordHash(request.Password , user.PasswordHash , user.PasswordSalt))
+            {
+                return BadRequest("Incorrect password");
+            }
+
+            return Ok("Login successfull");
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+        {
+            using (var hmac = new HMACSHA512(storedSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(storedHash);
+            }
+        }
+
+    }
+}
